eb53e4e79d8c1eb736b79a0dc1031eb69a287948
Kevin Oliveira <kevinklgvg@gmail.com>
2026-07-27 10:28:12 -0300

    add adpater prisma

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,7 +1,12 @@
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@/generated/prisma';
+import { env } from '@/schemas/env.schema';
+
+const adapter = new PrismaNeon({ connectionString: env.DATABASE_URL });
 
 export const prisma = new PrismaClient({
-  log: ['query', 'warn', 'error'],
-})
+  adapter,
+  log: env.NODE_ENV === 'production' ? ['warn', 'error'] : ['query', 'warn', 'error'],
+});
 
-export default {prisma};
+export default { prisma };
